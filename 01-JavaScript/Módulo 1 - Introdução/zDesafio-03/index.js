09befdedd86510f98ffccad4e4f806d53827caed
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-11 23:13:12 -0300

    zDesafio 03

---
+++ 01-JavaScript/Módulo 1 - Introdução/zDesafio-03/index.js
@@ -0,0 +1,8 @@
+function temHabilidade(skills) {
+
+    console.log((skills.indexOf('Javascript') > -1) ? true : false);
+
+}
+var skills = ["Javascript", "ReactJS", "React Native"];
+temHabilidade(skills); // true ou false
+   
