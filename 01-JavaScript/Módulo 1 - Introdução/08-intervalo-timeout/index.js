14c643e836d10a1bd1eac4a9bdb648acb3957c99
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-11 23:12:14 -0300

    interval timeout

---
+++ 01-JavaScript/Módulo 1 - Introdução/08-intervalo-timeout/index.js
@@ -0,0 +1,10 @@
+
+function exibeAlgo () {
+    console.log('Hello World');
+}
+
+setInterval(exibeAlgo, 1000);
+// Vai executar a cada 1s
+
+setTimeout(exibeAlgo, 1000);
+// vai ter um delay, e depois ele executa
