c7d45d54b6b9a3017d9a22c390ebc20b2e7771b4
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-11 23:11:26 -0300

    oper.Logicos

---
+++ 01-JavaScript/Módulo 1 - Introdução/05-operadores-logicos/index.js
@@ -0,0 +1,15 @@
+
+// AND, OR, NOT
+// &&, ||, !=
+
+var sexo = 'F', idade = 10;
+
+if (sexo == 'F' && idade >= 18) {
+    console.log('OK');
+}
+
+if (sexo !== 'M') {
+    console.log('ok');
+}
+
+var masculino = (sexo === 'M'); // retornar true ou false
