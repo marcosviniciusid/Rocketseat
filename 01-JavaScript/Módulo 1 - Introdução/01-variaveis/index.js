b66b250771ea61267d765048adbba79aa09effa5
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-11 23:10:36 -0300

    variaveis

---
+++ 01-JavaScript/Módulo 1 - Introdução/01-variaveis/index.js
@@ -0,0 +1,16 @@
+var nome = "Marcos"; // String
+var idade = 23; // Number
+var peso = 62.9; // Float
+var humano = true; // Booleano
+
+var alunos = ['Marcos', 'Vinicius', 'Gabriel']; // Vetor
+
+var aluno = { // Objeto
+    nome: 'Marcos',
+    idade: '22',
+    peso: 63,
+    humano: true
+}
+
+console.log(alunos[1]);
+console.log(aluno.nome);
