6ba0658b0aac06735395d88615ab56ff3a8f83f0
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-11 23:11:55 -0300

    estrutura repeticao

---
+++ 01-JavaScript/Módulo 1 - Introdução/07-estrutura-repeticao/index.js
@@ -0,0 +1,16 @@
+
+// for, while
+
+for (var i = 0; i < 100; i++) {
+    // Código
+    console.log(i);
+}
+// Utiliza-se sempre que sabe a quantidade
+
+var j = 0;
+while (j <= 100) {
+    console.log(j);
+    
+    j++;
+}
+// Utiliza-se quando não se sabe o final
