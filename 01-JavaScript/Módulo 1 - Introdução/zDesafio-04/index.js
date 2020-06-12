b2d56346c315eedc9c9cd55e9b1ab9ce00a7aee9
Marcos Silva <marcosviniciusid@outlook.com>
2020-06-11 23:13:20 -0300

    zDesafio 04

---
+++ 01-JavaScript/Módulo 1 - Introdução/zDesafio-04/index.js
@@ -0,0 +1,27 @@
+function experiencia(anos) {
+    switch(anos) {
+        case 0:
+        case 1:
+            console.log('Iniciante');
+            break;
+        case 2:
+        case 3:
+            console.log('Intermediário');
+            break;
+        case 4:
+        case 5:
+        case 6:
+            console.log('Avançado');
+            break;
+        default:
+            console.log('Jedi Master');
+    }
+}
+
+var anosEstudo = 4;
+experiencia(anosEstudo);
+// De 0-1 ano: Iniciante
+// De 1-3 anos: Intermediário
+// De 3-6 anos: Avançado
+// De 7 acima: Jedi Master
+   
